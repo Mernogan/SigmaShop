30903e462edf93078d9e908ca4f2c3188cb3f8c3
Mernogan <mernogan@gmail.com>
2025-02-22 12:00:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -231,4 +231,4 @@ if __name__ == "__main__":
         
         db.create_all()
     port = int(os.environ.get('PORT', 10000))
-    app.run(debug=False, port=port)
+    app.run(host='0.0.0.0', port=10000, debug=True)
